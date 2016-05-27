17bbdf7059bde7be1c29381052ff4a8aeeb02d3b
Rich Greenhill <rgnet1@yahoo.com>
2016-05-26 21:00:03 -0700

    add ctrl, some actions

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+var QUESTIONS = [
+   new Question('Do ye like yer drinks strong?', 'strong'),
+   new Question('Do ye like it with a salty tang?', 'salty'),
+   new Question('Are ye a lubber who likes it bitter?', 'bitter'),
+   new Question('Would ye like a bit of sweetness with yer poison?', 'sweet'),
+   new Question('Are ye one for a fruity finish?', 'fruity')
+];
+
 function Question(text, category){
   this.text = text;
   this.category = category;
@@ -11,14 +19,6 @@ function Ingredient(name, category){
 function Pantry(ingredients){
   this.ingredients = ingredients;
 }
-
-var QUESTIONS = [
-   new Question('Do ye like yer drinks strong?', 'strong'),
-   new Question('Do ye like it with a salty tang?', 'salty'),
-   new Question('Are ye a lubber who likes it bitter?', 'bitter'),
-   new Question('Would ye like a bit of sweetness with yer poison?', 'sweet'),
-   new Question('Are ye one for a fruity finish?', 'fruity')
-];
 
 var myPantry = new Pantry(
   [
@@ -41,6 +41,48 @@ var myPantry = new Pantry(
 );
 
 /********************
+ * START: Bartender Class
+ ********************/
+function Bartender(questions){
+  this.running = false;
+  this.questions = questions;
+  this.currQ = null;
+  this.userPreferences = {};
+}
+
+Bartender.prototype.startQuestions = function(){
+  var q;
+  
+  this.running = true;
+  this.currQ = 0;
+  q = this.questions[this.currQ];  
+  
+  return {
+    text: q.text,
+    category: q.category 
+  };
+};
+
+Bartender.prototype.nextQuestion = function(){
+  var q;
+  
+  if (this.running && this.currQ !== this.questions.length - 1 ) {
+    this.currQ++;
+    q = this.questions[this.currQ];  
+    return {
+      text: q.text,
+      category: q.category
+    };
+  } else {
+    return false;
+  }
+}
+/********************
+ * END: Bartender Class
+ ********************/
+
+
+/********************
  * START: VIEW Class
  ********************/
 function View(){
@@ -50,14 +92,16 @@ View.prototype.templates = function(template){
   switch(template){
     case 'question':
       return `
-        <h3>${this.text}</h3>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="user-answer" id="user-answer-true">YES</div>
-            </div>
-            <div class="col-sm-6">
-              <div class="user-answer" id="user-answer-false">NO</div>
+        <div id="question" data-category="${this.category}">
+          <h3>${this.text}</h3>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="user-answer user-answer-true">YES</div>
+              </div>
+              <div class="col-sm-6">
+                <div class="user-answer user-answer-false">NO</div>
+              </div>
             </div>
           </div>
         </div>
@@ -69,17 +113,57 @@ View.prototype.render = function(el, template, data) {
   var html = this.templates.call(data, template)
   el.html(html);
 };
-
 /********************
  * END: VIEW CLASS
  ********************/
 
+/***********************
+ * START: CONTROLLER CLASS
+ ***********************/
+function Controller(model,view){
+  this.model = model;
+  this.view = view;
+}
+
+Controller.prototype.init = function(){
+  var response = this.model.startQuestions();
+  this.view.render($('.question-area'), 'question', response);
+};
+
+Controller.prototype.submitAnswer = function(response){
+  // add answer to preferences
+  this.model.userPreferences[response.category] = response.userAnswer;
+  
+  // move to next question
+  var res = this.model.nextQuestion();
+  
+  // if last question, create drink!
+  if (res){
+    this.view.render($('.question-area'), 'question', res);
+  } else {
+    this.createDrink();
+  }
+};   
+ 
+/***********************
+ * END: CONTROLLER CLASS
+ ***********************/
+ 
+
 var view = new View();
-
-view.render($('.question'), 'question', {text: QUESTIONS[0].text});
-
+var bartender = new Bartender(QUESTIONS);
+var ctrl = new Controller(bartender, view);
 
 $(function(){
+  ctrl.init();
   
+  $('#main').on('click', '.user-answer', function(e){
+    var response = { 
+      category: $(this).closest('#question').data('category'), 
+      userAnswer: e.target.className.includes('user-answer-true')
+    };
+    
+    ctrl.submitAnswer(response);
+  });
 });
 
